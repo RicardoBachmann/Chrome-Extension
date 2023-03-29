e69ff4f16511a39a5ac0f6b7a7dcce7e539f525a
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-03-29 17:19:33 +0200

    Create a tabBtn and use Chrome API to get and save the tab

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
+
 
 //JSON.parse turns a String into a Array
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
@@ -11,6 +13,20 @@ if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){
+    //Use the chrome API to get the current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+        
+    })
+})
+
+
+
+
 
 function render(leads) {
     let listItems = ""
